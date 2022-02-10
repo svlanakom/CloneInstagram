45e1a1169112e1948b5091075c72143704aad77e
Svetlana <svetakom.222@gmail.com>
2022-02-10 14:22:35 +0200

    Form

--- validator.js
+++
@@ -1,5 +0,0 @@
-const VALIDATOR = {
-    'required': (value, param) => !!(value.lenght === 0),
-    'min-length': (value, param) => !!(value.lenght < param),
-    'max-length': (value, param) => !!(value.length > param),
-}
