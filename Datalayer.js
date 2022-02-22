622aa403e2a05b2e3d286f7a3ffce22a6600a1f1
Svetlana <svetakom.222@gmail.com>
2022-02-22 16:35:36 +0200

    try2

--- Datalayer.js
+++ Datalayer.js
@@ -1,23 +1,23 @@
-// class Datalayer{
+class Datalayer{
 
-//     add( users ){
-//         return localStorage.getItem(tableName,JSON.stringify(result))
-//     }
-//    get(key,tableName){
-//        return this.getAll(tableName)[key]
+    add( users ){
+        return localStorage.getItem(tableName,JSON.stringify(result))
+    }
+   get(key,tableName){
+       return this.getAll(tableName)[key]
 
-//    }
-//    getAll(tableName){
-//        return JSON.parse(localStorage.getItem(tableName)) || {};
-//    }
-//   delete(users){
-//       return localStorage.removeItem(tableName, JSON.stringify(obj))
-//   }
-//   update(){
-//      //
-//   }
+   }
+   getAll(tableName){
+       return JSON.parse(localStorage.getItem(tableName)) || {};
+   }
+  delete(users){
+      return localStorage.removeItem(tableName, JSON.stringify(obj))
+  }
+  update(){
+     //
+  }
 
-// }
+}
 
-// export default Datalayer
+export default Datalayer
 
