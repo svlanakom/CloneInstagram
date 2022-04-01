9ccfc8e9a7605d0b16463ff63b7de884662e3df7
Svetlana <svetakom.222@gmail.com>
2022-04-01 11:43:31 +0300

    change

--- Form.js
+++ Form.js
@@ -1,20 +1,16 @@
 export default class Form {
-    constructor(elem, fields, errorElements) {
-        this.elem = elem;
-        this.fields = fields;
-        this.errorElements = errorElements;
-    }
+  constructor(elem, fields, errorElements) {
+    this.elem = elem;
+    this.fields = fields;
+    this.errorElements = errorElements;
+  }
 
-    clearInput() {
-        for (const name in this.fields) {
-            if (Object.hasOwnProperty.call(this.fields, name)) {
-                this.fields[name].value = "";
-            }
-        }
-        for (const name in this.errorElements) {
-            if (Object.hasOwnProperty.call(this.errorElements, name)) {
-                this.errorElements[name].textContent = "";
-            }
-        }
+  clearInput() {
+    for (const name in this.fields) {
+      this.fields[name].value = "";
     }
+    for (const name in this.errorElements) {
+      this.errorElements[name].textContent = "";
+    }
+  }
 }
