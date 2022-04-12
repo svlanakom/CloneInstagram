ee58d0079b0acae5c758548ce13c8f8688a042ed
Svetlana <svetakom.222@gmail.com>
2022-04-12 18:53:32 +0300

    classEditForm

--- Form.js
+++ Form.js
@@ -3,18 +3,21 @@ export default class Form {
     this.elem = elem;
     this.fields = fields;
     for (const key in this.fields) {
-      const element = this.fields[key];
-      element.addEventListener("input", () => {
-        element.nextElementSibling.innerText = "";
-        if (!element.checkValidity())
-          element.nextElementSibling.innerText = element.dataset.errorMessage;
-      });
+      if (Object.hasOwnProperty.call(this.fields, key)) {
+        const element = this.fields[key];
+        element.addEventListener("input", () => {
+          element.nextElementSibling.innerText = "";
+          if (!element.checkValidity())
+            element.nextElementSibling.innerText = element.dataset.errorMessage;
+        });
+      }
     }
   }
-
   clearInput() {
     for (const name in this.fields) {
-      this.fields[name].value = "";
+      if (Object.hasOwnProperty.call(this.fields, name)) {
+        this.fields[name].value = "";
+      }
     }
   }
 }
