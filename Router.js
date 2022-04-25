22024dc442998784512a5b1a92acc42e9a7b32de
Svetlana <svetakom.222@gmail.com>
2022-04-25 12:53:42 +0300

    rout

---
+++ Router.js
@@ -0,0 +1,23 @@
+
+
+// document.querySelector("click", (e) => {
+//   const { target } = e.target;
+//   if (!target.matches("nav a")) {
+//     return;
+//   }
+//   e.preventDefault();
+//   urlRoute();
+// });
+
+// class Router {
+
+//     constructor(routes) {
+//         this.routes = routes;
+//         this.loadInitialRoute();
+// }
+
+// loadRoute(urlSegment) {
+//     const matchedRoute = this.matchUrlRoute(urlSegment);
+//     const url =`/${urlSegment.join('/')}`;
+//     history.pushState({}, '', url);
+// }
---
+++ routes.js
@@ -0,0 +1,10 @@
+const routes = [
+    {
+      path: "/",
+      template: "<h1>Home</h1>",
+    },
+    {
+      path: "/",
+      template: "<h1>About</h1>",
+    },
+  ];
