a453030326950154bc41ef6d515569de47cf2ce5
Svitlana Komlieva <svitlanakomlieva@coherentsolutions.com>
2022-05-07 18:44:09 +0300

    rout

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 import App from "./App.js";
+import Router from "./Roter.js";
+import { phone } from "./constants.js";
 
 const app = new App();
+
+const routes = {
+    home: {
+        template: "/templates/index.html",
+        title: "Home",
+        controller: () => {
+            app.updateTable();
+        },
+    },
+    about: {
+        template: "/templates/about.html",
+        title: "About Us",
+        controller: () => {
+            
+        },
+    },
+    contact: {
+        template: "/templates/contact.html",
+        title: "Contact",
+        controller: () => {
+            document.querySelector("#phone").innerHTML += phone;
+        },
+    },
+};
+
+new Router(routes);
